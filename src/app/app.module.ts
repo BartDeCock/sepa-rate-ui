a5ed1d566f384e747de28ba58d78dd895d18417b
nielsjani <NielsJani>
2019-02-01 16:02:05 +0100

    add user login screen + connection to backend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,22 @@ import {HttpClientModule} from '@angular/common/http';
 import {FormsModule} from '@angular/forms';
 import {AppRoutingModule} from './app-routing.module';
 import {SeparationModule} from './separation/separation.module';
+import {UserModule} from './user/user.module';
+import {CookieModule} from 'ngx-cookie';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    CookieModule.forRoot(),
     FormsModule,
     BrowserModule,
     HttpClientModule,
-    AppRoutingModule,
-    SeparationModule
+    UserModule,
+    SeparationModule,
+    //  App routing module should always be last in list. This module contains the fallback paths
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
